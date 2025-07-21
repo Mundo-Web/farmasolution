de1c5632f26651885c1766a9af7df606dcb50d6d
brad-basilio <brad.basilioppy@gmail.com>
2025-07-21 14:41:42 -0500

    add

--- resources/js/Components/Tailwind/Menu.jsx
+++ resources/js/Components/Tailwind/Menu.jsx
@@ -4,6 +4,7 @@ import React from "react"
 
 const MenuSimple = React.lazy(() => import('./Menu/MenuSimple'))
 const MenuBananaLab = React.lazy(() => import('./Menu/MenuBananaLab'))
+const MenuExpertFarma = React.lazy(() => import('./Menu/MenuExpertFarma'))
 const Menu = ({ data, which, items, generals = [], cart, setCart, pages }) => {
   const getMenu = () => {
     switch (which) {
@@ -12,7 +13,11 @@ const Menu = ({ data, which, items, generals = [], cart, setCart, pages }) => {
         return <MenuSimple data={data} items={items} cart={cart} setCart={setCart} pages={pages} />
         case 'MenuBananaLab':
           return <MenuBananaLab data={data} items={items} cart={cart} setCart={setCart} pages={pages} />
-      default:
+      
+      case 'MenuExpertFarma':
+        return <MenuExpertFarma data={data} items={items} cart={cart} setCart={setCart} pages={pages} generals={generals} />
+      
+          default:
         return <div className="w-full px-[5%] replace-max-w-here p-4 mx-auto">- No Hay componente <b>{which}</b> -</div>
     }
   }
