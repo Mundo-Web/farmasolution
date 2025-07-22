727ecd3c5f3bc62fe427e931b1a8803ff61a6f1b
brad-basilio <brad.basilioppy@gmail.com>
2025-07-22 11:32:19 -0500

    expert farma blog - about -filter -checkout first

--- resources/js/Components/Tailwind/Categories/InfiniteCategory.jsx
+++ resources/js/Components/Tailwind/Categories/InfiniteCategory.jsx
@@ -28,7 +28,7 @@ const InfiniteCategory = ({ items, data }) => {
     }, [swiperInstance]);
 
     return (
-        <section className="py-12">
+        <section className={`${data?.backgroundColor ? data.backgroundColor : 'bg-white'} py-12`}>
             <div className="w-full px-[5%] py-[2.5%] mx-auto">
                 <h2 className="text-[28px] md:text-4xl font-bold pb-4 mb-8 font-font-secondary border-b customborder-neutral-light">
                     {data?.title}
