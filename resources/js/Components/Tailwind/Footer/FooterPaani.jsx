c50396a6bec905a187ca8d5e6ab77a194eeb148c
Papayadev2024 <diegomartinez.papaya@gmail.com>
2025-07-24 15:19:30 -0500

    final

--- resources/js/Components/Tailwind/Footer/FooterPaani.jsx
+++ resources/js/Components/Tailwind/Footer/FooterPaani.jsx
@@ -200,7 +200,7 @@ export default function FooterPaani({ data = footerData, generals, socials }) {
                                             key={index}
                                             content={`Ver ${social.name} en ${social.description}`}>
 
-                                            <a key={index} className="text-xl w-8 h-8 flex items-center justify-center  bg-white rounded-full p-2 customtext-primary" href={social.url} target="_blank" rel="noopener noreferrer">
+                                            <a key={index} className="text-xl w-8 h-8 flex items-center justify-center  bg-white rounded-full p-2 customtext-primary" href={social.link} target="_blank" rel="noopener noreferrer">
                                                 <i className={social.icon} />
                                             </a>
                                         </Tippy>
