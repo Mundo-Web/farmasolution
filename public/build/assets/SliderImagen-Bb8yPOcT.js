74c237a71196aae44372133e1652cfb6760beabf
Papayadev2024 <diegomartinez.papaya@gmail.com>
2025-07-31 11:32:03 -0500

    final

---
+++ public/build/assets/SliderImagen-Bb8yPOcT.js
@@ -0,0 +1,1 @@
+import"./AboutSimple-Cf8x2fCZ.js";import"./BlogCarousel-DO3sPssn.js";/* empty css               */import"./BlogCarrusel-7hiZQSSW.js";import"./index-BOnQTV8N.js";import"./index-yBjzXJbu.js";import"./_commonjsHelpers-D6-XlEtG.js";
---
+++ public/build/assets/TestimonialsPaani-BByFYhei.js
@@ -0,0 +1,1 @@
+import"./AboutSimple-Cf8x2fCZ.js";import"./BlogCarousel-DO3sPssn.js";import"./index-BOnQTV8N.js";/* empty css               */import{G as o}from"./Global-DHhdk9zW.js";import"./index-yBjzXJbu.js";import"./_commonjsHelpers-D6-XlEtG.js";`${o.APP_CORRELATIVE}`;
